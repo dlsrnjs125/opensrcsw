755a9cf02ae9c24dca4f53b289012880b88efb7c
dlsrnjs125 <dlsrnjs125@naver.com>
2020-04-24 12:49:35 +0900

    0 input -> 2~9dan output

--- java/src/opensrcsw/gugudan.java
+++ java/src/opensrcsw/gugudan.java
@@ -7,8 +7,19 @@ public class gugudan {
 		 System.out.print("원하는 단을 입력하세요 : ");
 		 Scanner sc = new Scanner(System.in);
 		 int a = sc.nextInt();
+		 if (a == 0) {
+			 for (int j = 2; j <=9; j++) {
+				 System.out.print(j + "단 : ");
+				 for (int k =1; k <=9; k++) {
+					 System.out.print(j + "*" + k + "=" + (j*k)+ " ");
+				 }
+				 System.out.println();
+			 }
+		 }
+		 else if (a > 0) {
 		 for (int i=1; i<10; i++) {
 			 System.out.println(a + " * "+ i + " = " + (a*i));
+		 }
 		 }
 	 }
 
